90af1834d58020f219104332a749a93d9bf4f385
vartanian-cmd <vartanian2004@gmail.com>
2026-03-13 11:27:24 -0700

    Create Home.jsx

---
+++ pages/Home.jsx
@@ -0,0 +1,34 @@
+
+export default function Home() {
+  return (
+    <main className="page-wrap">
+
+      <section className="section" style={{ paddingTop: '3rem' }}>
+        <div className="container">
+          <div className="hero">
+            <div className="hero-content animate-fadeUp">
+              <p className="hero-eyebrow">Computer Science Undergraduate</p>
+              <h1>
+                Alex Vartanian<br />
+              </h1>
+              <p className="hero-desc">
+                I'm Alex Vartanian — a computer science undergraduate at California State University Bakersfield. I have a passion for software development. I'm actively seeking internship opportunities and Project Ideas.
+              </p>
+            </div>
+
+            <div className="hero-img-wrap animate-fadeIn delay-3">
+              <div className="hero-img-frame">
+                <img
+                  src="image here"
+                />
+              </div>
+              <div className="hero-img-decor" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+    </main>
+  );
+}
