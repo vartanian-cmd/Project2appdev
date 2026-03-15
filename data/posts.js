594b01a5f934e4acd41225eae8a1153d35c0fccf
vartanian-cmd <vartanian2004@gmail.com>
2026-03-15 08:32:42 -0700

    Update posts.js

--- data/posts.js
+++ data/posts.js
@@ -1,1 +1,15 @@
+const posts = [
+  {
+    id: 1,
+    slug: "test-blog",
+    title: "test",
+    date: "March 15, 2025",
+    dateISO: "2025-03-015",
+    excerpt: "please work",
+    coverImage: "",
+    tags: ["React"],
+    content: "help",
+  },
+  ];
 
+export default posts;
