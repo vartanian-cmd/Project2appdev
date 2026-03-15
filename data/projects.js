20a7d567d7174c437d5b8dc77dfa56bacb063d3c
vartanian-cmd <vartanian2004@gmail.com>
2026-03-15 08:26:58 -0700

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -1,1 +1,18 @@
+import tutorcenter from '../assets/tutorcenter.png';
 
+const projects = [
+  {
+    id: 1,
+    slug: "tutoring-database-system",
+    title: "Tutoring Center Database System",
+    description: "",
+    excerpt: "Fully functional database system with a user-friendly GUI for displaying data, querying, data removal and entry",
+    image: tutorcenter,
+    tech: ["MySQL", "PHP", "CSS Modules"],
+    github: "https://github.com/npaul-2/TutoringCenter/tree/main",
+    year: "2025",
+    role: "Developer",
+  },
+];
+
+export default projects;
