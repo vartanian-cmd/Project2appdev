44f71620c13078ae5ac51eb58a52d0934ab4d151
vartanian-cmd <vartanian2004@gmail.com>
2026-03-15 22:46:20 -0700

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -1,15 +1,16 @@
 import tutorcenter from '../assets/tutorcenter.png';
 import Pacman from '../assets/Pacman.png';
+import passwordmanger from '../assets/passwordmanger.png';
 
 const projects = [
   {
     id: 1,
     slug: "tutoring-database-system",
     title: "Tutoring Center Database System",
-    description: "",
+    description: "Together with 3 other students we devloped the front and back end of a working Database. In the database we included various tools for both Tutors and Students to assist in handling sessions between the two groups. Our database handles displaying data, data entry and deletion, updating data, login pages for both groups, and more. Above is a screenshot of the GUI with a table displaying that can be updated through the GUI.",
     excerpt: "Fully functional database system with a user-friendly GUI for displaying data, querying, data removal and entry",
     image: tutorcenter,
-    tech: ["MySQL", "PHP", "CSS Modules"],
+    tech: ["MySQL", "PHP", "CSS Modules","VS Code Studio"],
     github: "https://github.com/npaul-2/TutoringCenter/tree/main",
     year: "2025",
     role: "Developer",
@@ -18,11 +19,23 @@ const projects = [
     id: 2,
     slug: "A*-Search-Pacman",
     title: "Pacman using A* Search Algorithm",
-    description: "",
+    description: "This was a solo project made to display a usage for the popular A* Search algorithm. In this project I recreated the popular game Pacman with the enemies (Ghosts) using an A* search alogrithm to locate and track down the player (Pacman). ",
     excerpt: "A recreation of the popular game Pacman using the A* Search algorithm for enemy pathfinding",
     image: Pacman,
-    tech: ["Unity", "C#",],
+    tech: ["Unity", "C#","VS Code Studio"],
     github: "https://github.com/vartanian-cmd/A-Pacman",
+    year: "2025",
+    role: "Developer",
+  },
+    {
+    id: 3,
+    slug: "password-manager",
+    title: "Password Manager (In Progress)",
+    description: "This project is currently in progress, the purpose of this software would be to finally never forget a password again. This password manager would be installed both on PC and Mobile devices and would be connected between the two sharing passwords from both devices so you never have login issues again.",
+    excerpt: "A personalized password manager that works accross all devices and all applications",
+    image: passwordmanger,
+    tech: ["VS Code Studio", "Java",],
+    github: "https://github.com",
     year: "2025",
     role: "Developer",
   },
